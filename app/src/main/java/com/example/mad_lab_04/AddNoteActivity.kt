8fe60f6fb76fd5a26e52b2e64c4b7c174e09d1e5
IT22099686 <127779990+Nadun25@users.noreply.github.com>
2024-05-11 01:52:59 +0530

    create more funtionalities and crete UI

--- app/src/main/java/com/example/mad_lab_04/AddNoteActivity.kt
+++ app/src/main/java/com/example/mad_lab_04/AddNoteActivity.kt
@@ -3,7 +3,7 @@ package com.example.mad_lab_04
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AddNewActivity : AppCompatActivity() {
+class AddNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new)
--- app/src/main/java/com/example/mad_lab_04/MainActivity.kt
+++ app/src/main/java/com/example/mad_lab_04/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mad_lab_04
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mad_lab_04.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
